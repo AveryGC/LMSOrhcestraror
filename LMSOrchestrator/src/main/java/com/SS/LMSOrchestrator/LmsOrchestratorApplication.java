1b9bd7f1ab83b4516b5a986248e9f92d276dc525
AverySSgc <avery.corbett@smoothstack.com>
2020-02-25 21:48:17 -0500

    updated with eureka client settings.
    uses eureka to talk to micro services,
    added consumes for all and produces for jsoncontroller

--- LMSOrchestrator/src/main/java/com/SS/LMSOrchestrator/LmsOrchestratorApplication.java
+++ LMSOrchestrator/src/main/java/com/SS/LMSOrchestrator/LmsOrchestratorApplication.java
@@ -2,14 +2,25 @@ package com.SS.LMSOrchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaClient
 public class LmsOrchestratorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LmsOrchestratorApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
 	}
 
 }
