af7426f8102221517ef6539bd18f3ff269e4b303
AverySSgc <avery.corbett@smoothstack.com>
2020-02-27 15:44:48 -0500

    updated with proper bean placement

--- LMSOrchestrator/src/main/java/com/SS/LMSOrchestrator/LmsOrchestratorApplication.java
+++ LMSOrchestrator/src/main/java/com/SS/LMSOrchestrator/LmsOrchestratorApplication.java
@@ -2,11 +2,7 @@ package com.SS.LMSOrchestrator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
@@ -17,10 +13,5 @@ public class LmsOrchestratorApplication {
 		SpringApplication.run(LmsOrchestratorApplication.class, args);
 	}
 	
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-	    return new RestTemplate();
-	}
 
 }
